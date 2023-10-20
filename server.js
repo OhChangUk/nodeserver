6bc268bd56eaa349c1f1e9622ac5a5551baf7f1c
오창욱 <o2556@naver.com>
2023-10-20 10:42:14 +0900

    저장

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.use(express.urlencoded({extended: true}))
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+const bcrypt = require('bcrypt')
+
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const MongoStore = require('connect-mongo')
+
+app.use(passport.initialize())
+app.use(session({
+    secret : "암호화에 쓸 비번", // 세션문서의 암호화
+    resave : false, // 유저가 서버로 요청할 때마다 갱신할건지
+    saveUninitialized : false, // 로그인 안해도 세션 만들건지
+    cookie: {maxAge: 60 * 60 * 1000}, //1시간 후 쿠키에 있는 섹션 사라짐
+    store : MongoStore.create({
+        mongoUrl : `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PW}@cluster0.oz1pnru.mongodb.net/`,
+        dbName : "board"
+    })
+}))
+app.use(passport.session())
+
 app.set("view engine", "ejs");
 // ejs사용 세팅
 app.use(express.static(__dirname + '/public'))
@@ -22,9 +42,9 @@ const url = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PW}@c
 new MongoClient(url).connect().then((client)=>{
     db = client.db("board");
     sample = client.db("sample_restaurants");
-    console.log("DB 연결 완료!")
+    // console.log("DB 연결 완료!")
     app.listen(process.env.SERVER_PORT, ()=>{
-        console.log(`${process.env.SERVER_PORT}번 포트 서버 실행`)
+        // console.log(`${process.env.SERVER_PORT}번 포트 서버 실행`)
     })
     
 }).catch((error)=>{
@@ -49,8 +69,19 @@ app.get('/about',(req,res)=>{
 })
 app.get('/list',async (req,res)=>{
 
-    const result = await db.collection("notice").find().toArray()
-    console.log(result[0])
+    const result = await db.collection("notice").find().limit(5).toArray()
+    // limit(n) = 페이지네이션 n개 보여줌
+    // console.log(result[0])
+
+    res.render("list.ejs", {
+        data : result
+    })
+})
+app.get('/list/:id',async (req,res)=>{
+
+    const result = await db.collection("notice").find().skip((req.params.id - 1)* 6).limit(5).toArray()
+    // limit(n)  = 페이지네이션 n개 보여줌
+    // console.log(result[0])
 
     res.render("list.ejs", {
         data : result
@@ -76,7 +107,7 @@ app.get('/test',(req,res)=>{
 })
 
 app.post('/add',async (req,res)=>{
-    console.log(req.body)
+    // console.log(req.body)
     await db.collection("notice").insertOne({
         title: req.body.title,
         content: req.body.content
@@ -89,7 +120,7 @@ app.put('/edit',async (req,res)=>{
     // updateOne({문서},{
     // $set : {원하는 키 : 변경값}
     // })
-    console.log(req.body)
+    // console.log(req.body)
     await db.collection("notice").updateOne({
         _id : new ObjectId(req.body._id)
     }, {
@@ -104,7 +135,7 @@ app.put('/edit',async (req,res)=>{
 
 
 app.get('/delete/:id',async (req,res)=>{
-    const result = await db.collection("notice").deleteOne({
+    await db.collection("notice").deleteOne({
         _id : new ObjectId(req.params.id)
     })
     res.redirect('/list')
@@ -118,6 +149,83 @@ app.get('/edit/:id',async (req,res)=>{
         data: result
     })
 })
+
+passport.use(new LocalStrategy({
+    usernameField : 'userid',
+    passwordField : 'password'
+},async (userid,password,cb ) => {
+    // cb - 도중에 실행
+    let result = await db.collection("users").findOne({
+        userid : userid
+    })
+
+    if(!result){
+        return cb(null, false, {message: "아이디나 비밀번호가 일치하지 않음"})
+    }
+    const passChk = await bcrypt.compare(password, result.password)
+    console.log(passChk)
+    if(passChk){
+        return cb(null, result)
+    }else{
+        return cb(null, false, {message: "아이디나 비밀번호가 일치하지 않음"})
+    }
+}))
+
+passport.serializeUser((user,done)=>{
+    process.nextTick(()=>{
+        // done(null, 세션에 기록할 내용)
+        done(null, {id : user._id, userid : user.userid})
+    })
+    // 비동기식 코드
+})
+
+passport.deserializeUser(async (user,done)=>{
+    let result = await db.collection("users").findOne({
+        _id: new ObjectId(user.id)
+    })
+    delete result.password
+    process.nextTick(()=>{
+        done(null, result)
+    })
+})
+
+app.get('/login', (req,res) => {
+    res.render("login.ejs")
+})
+app.post('/login', async(req,res,next) => {
+    // console.log(req.body)
+    passport.authenticate('local', (error, user, info) => {
+        if(error) return res.status(500).json(error)
+        // 500에러 - 서버 에러
+        if(!user) return res.status(401).json(info.message)
+        // info - 실패했을 때 데이터
+        // user - 성공했을 때 데이터
+        req.logIn(user, (error) => {
+            if(error) return next(error)
+            res.redirect('/')
+        })
+    })(req,res,next)
+})
+
+app.get('/register', (req,res) => {
+    res.render("register.ejs")
+})
+
+app.post('/register', async(req,res) => {
+    let hashPass = await bcrypt.hash(req.body.password, 10)
+    // console.log(hashPass)
+    try{
+        await db.collection("users").insertOne({
+            userid: req.body.userid,
+            password: hashPass
+        })
+    }catch(error){
+        console.log(error)
+    }
+    // res.send("성공!")
+    res.redirect('/list')
+})
+
 // 1. Uniform Interface
 // 여러 URL과 METHOD는 일관성이 있어야 하며, 하나의 URL에서는 하나의 데이터만 가져오게 디자인하며, 간결하고 예측 가능한 URL과 METHOD를 만들어야 한다.
 // 동사보다는 명사 위주
